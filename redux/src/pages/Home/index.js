0ae7ca99aa707ac28658d663b03743f6c6e09038
Joao Pedro <802.018@alunos.unigran.br>
2020-12-16 17:06:51 -0300

    Json server + estilização da home

--- redux/src/pages/Home/index.js
+++ redux/src/pages/Home/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import api from "../../services/api";
+import { MdFlightTakeoff } from "react-icons/md";
+
+import './style.css';
 
 export default function Home() {
- return (
-   <div>
-        <h1>Home</h1>
-   </div>
- );
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    async function loadApi() {
+      const response = await api.get("trips");
+      setTrips(response.data);
+    }
+
+    loadApi();
+  }, []);
+
+  return (
+    <div className="box">
+      {trips.map((trip) => (
+        <li key={trip.id}>
+          <img src={trip.image} alt={trip.title}></img>
+          <strong>{trip.title}</strong>
+          <span>Status: {trip.status ? "Disponivel" : "Indisponivel"}</span>
+
+          <button type="button" onClick={() => {}}>
+            <div>
+              <MdFlightTakeoff size={16} color="#fff " />
+            </div>
+            <span>SOLICITAR A RESERVA AGORA MESMO!!!</span>
+          </button>
+        </li>
+      ))}
+    </div>
+  );
 }
