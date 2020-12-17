54b8473b289c270638d620f04315f88b95a47394
Joao Pedro <802.018@alunos.unigran.br>
2020-12-17 14:55:59 -0300

    Removendo os produtos

--- Aula-reduz/src/pages/Reservas/index.js
+++ Aula-reduz/src/pages/Reservas/index.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { MdDelete } from "react-icons/md";
 import "./style.css";
 
 export default function Reservas() {
   const reserves = useSelector(state=>state.reserve);
+  const dispatch = useDispatch();
 
+  function handleRemove(id){
+    dispatch({
+      type: 'REMOVE_RESERVE',
+      id,
+    })
+  }
 
 
   return (
@@ -20,7 +27,7 @@ export default function Reservas() {
   />
   <strong>{reserve.title}</strong>
   <span>Quantidade: {reserve.amount}</span>
-  <button type="button" onClick={() => {}}>
+  <button type="button" onClick={() => handleRemove(reserve.id)} >
     <MdDelete size={20} color="#191919" />
   </button>
 </div>
